bae1a2e349f1b53dcc91fb677522f6a679661db1
tais.amanda <tais.amanda@g.globo>
2023-03-14 18:08:05 -0300

    add: Solução do primeiro desafio com ruby

---
+++ desafio1.rb
@@ -0,0 +1,9 @@
+puts "hello ruby!"
+
+print "Digite seu nome:  "
+name = gets.chomp
+print "Digite seu sobrenome: "
+lastname = gets.chomp
+print "Digite sua idade: "
+age = gets.chomp
+puts "Olá #{name} #{lastname}! Seja bem vindo! Sua idade é #{age}." 
